2a3d2215321db4152686d21d6712c32db80fa6bb
Mathieu Nayrolles <mathieu.nayrolles@gmail.com>
2016-03-28 23:58:47 -0400

    comment & refactoring

--- simplex.js
+++ simplex.js
@@ -7,17 +7,14 @@ var linearExpression;
  * Checks if the input variable is a LP equation
  *
  * Here's a  valid input example :
- * Maximize p = (1/2)x + 3y + z + 4w subject to
+ * Maximize p = 0.5x + 3y + z + 4w subject to
  * x + y + z + w <= 40
- * 2x + y - z - w >= 10
- * w - y >= 10
+ * 2x + y - z - w <= 10
+ * w - y <= 10
  *
- * and the expected output
- * maximizep=(1/2)x+3y+z+4wsubjecttox+y+z+w<=402x+y-z-w>=10w-y>=10
- *
- * The output shall be feed to the lpParser
+ * The output shall be feed to the simplex algo
  */
-function checkInput(inputId){
+function createTableau(inputId){
 
   lpString = document.getElementById(inputId).value.toLowerCase();
   console.log(lpString);
@@ -32,9 +29,12 @@ function checkInput(inputId){
 
   console.log(linearExpression.toArray());
 
-  return lpString;
+  return linearExpression;
 }
 
+/**
+ * Create a Tableau given a linearString
+ */
 function Tableau(linearString){
 
   var linearStringSplitted = linearString.split(" subject to");
@@ -57,6 +57,45 @@ function Tableau(linearString){
   }
 }
 
+
+/**
+ * Creates a new Constraint
+ * @param  variables
+ * @param  coefVariables
+ * @param  op
+ * @param  string
+ * @param  rightHand
+ */
+function Constraint(variables, coefVariables, op, string, rightHand){
+
+  for(var i = 0; i < variables.length; i++){
+    this[variables[i]] = 0.0;
+  }
+
+  for(var y = 0; y < coefVariables.length; y++){
+    this[coefVariables[y].variable] = parseFloat(coefVariables[y].coefficient);
+  }
+
+  this.variables = variables;
+  this.op = op;
+  this.string = string;
+  this.rightHand = parseFloat(rightHand);
+
+  this.toArray = function(){
+    var array = [];
+    array[0] = this.rightHand;
+    for(var i = 0; i < variables.length; i++){
+      array[i+1] = this[this.variables[i]];
+    }
+    return array;
+  }
+}
+
+/**
+ * Extract all the variables inside a String
+ * @param  String equation
+ * @return Array
+ */
 function extractVariables(equation){
 
   var re = /([a-z]+)/g;
@@ -75,6 +114,12 @@ function extractVariables(equation){
 
 }
 
+/**
+ * Extracts the constantes
+ * @param  variables
+ * @param  constanteEquations
+ * @return [Constraint]
+ */
 function extractConstantes(variables, constanteEquations){
 
   var constantes = [];
@@ -118,31 +163,11 @@ function extractConstantes(variables, constanteEquations){
   return constantes;
 }
 
-function Constraint(variables, coefVariables, op, string, rightHand){
-
-  for(var i = 0; i < variables.length; i++){
-    this[variables[i]] = 0.0;
-  }
-
-  for(var y = 0; y < coefVariables.length; y++){
-    this[coefVariables[y].variable] = parseFloat(coefVariables[y].coefficient);
-  }
-
-  this.variables = variables;
-  this.op = op;
-  this.string = string;
-  this.rightHand = parseFloat(rightHand);
-
-  this.toArray = function(){
-    var array = [];
-    array[0] = this.rightHand;
-    for(var i = 0; i < variables.length; i++){
-      array[i+1] = this[this.variables[i]];
-    }
-    return array;
-  }
-}
-
+/**
+ * Extracts variables and coefficient from equation
+ * @param  equation
+ * @return
+ */
 function extractCoefficientedVariables(equation){
   var re = /(\-\s|\+\s)?(([0-9]+(.[0-9]+)?)?)([a-z]+)/g;
   var m;
